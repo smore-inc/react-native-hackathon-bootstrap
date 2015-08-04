14dac3ce66bd9dd1daa486afef338848ed2eab52
shlomiatar <shlomiatar@gmail.com>
2015-08-04 23:24:03 +0300

    progress

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
+/***
+ This file is a placeholder file, to fix an issue with the react-native bundler command
+
+ shlomiatar 24/7/15
  */
-'use strict';
 
-var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
-
-var HackApp = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('HackApp', () => HackApp);
+require('./ReactApp/index.ios');
