61498420117ba28e463d97ef791f9eef064fd1db
shlomiatar <shlomiatar@gmail.com>
2015-08-05 00:11:36 +0300

    Added support for the RUN_FROM_BUNDLE flag 

--- iOS/AppDelegate.m
+++ iOS/AppDelegate.m
@@ -17,40 +17,27 @@
 {
   NSURL *jsCodeLocation;
 
-  /**
-   * Loading JavaScript code - uncomment the one you want.
-   *
-   * OPTION 1
-   * Load from development server. Start the server from the repository root:
-   *
-   * $ npm start
-   *
-   * To run on device, change `localhost` to the IP address of your computer
-   * (you can get this by typing `ifconfig` into the terminal and selecting the
-   * `inet` value under `en0:`) and make sure your computer and iOS device are
-   * on the same Wi-Fi network.
-   */
+  
+  #ifdef RUN_FROM_BUNDLE
+    // Run code from the bundle
+    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  #else
+    // Use the code from the development server
+    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/ReactApp/index.ios.bundle"];
+  #endif
 
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
 
-  /**
-   * OPTION 2
-   * Load from pre-bundled file on disk. To re-generate the static bundle
-   * from the root of your project directory, run
-   *
-   * $ react-native bundle --minify
-   *
-   * see http://facebook.github.io/react-native/docs/runningondevice.html
-   */
-
-//   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-
+  // Show the status bar (we hid it for the launch screen)
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"HackApp"
                                                    launchOptions:launchOptions];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [[UIViewController alloc] init];
+  
+  // Set your app background color (use uicolor.io to pick your own)
+  rootView.backgroundColor = [UIColor colorWithRed:0.11 green:0.12 blue:0.13 alpha:1.0];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
